e950637b17bf04b4f1f928919a6f40af563398dc
robertc481 <49004135+robertc481@users.noreply.github.com>
2020-03-22 19:41:10 +0100

    prod build to deploy on gh pages

---
+++ build/precache-manifest.64dee8d8a97343e5599ec1c72aeb0b2e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3be4ab80c435b81be363c7d443f45fd7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6be454662510ce781c3b",
+    "url": "/static/css/main.8a409eaa.chunk.css"
+  },
+  {
+    "revision": "687ecb2db137ba2d397a",
+    "url": "/static/js/2.1474f0fc.chunk.js"
+  },
+  {
+    "revision": "233c8d7ff532fc2c0e62392e5ec28675",
+    "url": "/static/js/2.1474f0fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6be454662510ce781c3b",
+    "url": "/static/js/main.e2281fa7.chunk.js"
+  },
+  {
+    "revision": "97376c39d082c1726d6d",
+    "url": "/static/js/runtime-main.169331d0.js"
+  },
+  {
+    "revision": "03f5557af05b015dbcd3ca500eddbfff",
+    "url": "/static/media/scene.03f5557a.svg"
+  }
+]);
